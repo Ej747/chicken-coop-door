4282dd5cb27f4b081b702d2d1b24bdd3d8b38bf7
Ethan <pianofire3103@gmail.com>
2023-04-25 18:48:21 -0400

    adding more basic outline

---
+++ README.md
@@ -0,0 +1,2 @@
+just a note for future Ethan:
+    you should run/host the .php file rather than just the .html file and telling apache to run .html as .php
---
+++ scripting.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first PHP page</h1>
+
+<?php
+//this is where the php goes. yee :)
+echo "Hello World!";
+?>
+
+</body>
+</html>
